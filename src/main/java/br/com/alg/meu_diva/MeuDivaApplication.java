71bc85ce7a04245d420e7be081de3a7d194c3bdf
André Girão <agirao@unigex.com.br>
2025-10-02 09:13:20 -0300

    first commit

---
+++ src/main/java/br/com/alg/meu_diva/MeuDivaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alg.meu_diva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuDivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuDivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alg/meu_diva/MeuDivaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alg.meu_diva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuDivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
